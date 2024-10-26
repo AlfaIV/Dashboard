c416e4c89ffb075f2b786da53b3d38ad7bc2ee91
alfaiv <alfaiv@yandex.ru>
2024-10-26 23:22:56 +0300

    simple add graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BaseChartDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'RosAtom';
+  public lineChartData = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+  ];
+  public lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions = {
+    responsive: true
+  };
+  public lineChartLegend = true;
+  public lineChartType = 'line';
 }
