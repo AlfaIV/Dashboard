35a385504c7605578816a244e0e815c13481e04d
alfaiv <alfaiv@yandex.ru>
2024-10-28 20:09:50 +0300

    add bar-chart add request

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bar-chart',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './bar-chart.component.html',
-  styleUrl: './bar-chart.component.scss'
+  styleUrl: './bar-chart.component.scss',
 })
 export class BarChartComponent {
+  public barChartData: number[] = [];
+  public barChartLabels: string[] = [];
+  public barChartOptions = {
+    responsive: true,
+  };
+  public barChartLegend = true;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://api.binance.com/api/v3/ticker/24hr').subscribe({
+      next: (data: any) => {
+        let dataSelection = data
+          .sort((a: any, b: any) => b.volume - a.volume)
+          .slice(0, 10);
+        this.barChartData = dataSelection.map((d: any) => d.volume);
+        this.barChartLabels = dataSelection.map((d: any) => d.symbol);
+      },
+      error: (err: any) => {
+        console.error('Failed to fetch data', err);
+      },
+    });
+  }
 }
